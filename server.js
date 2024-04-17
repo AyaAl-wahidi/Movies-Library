6d1cf6b599592a23111a6d2d8fc807e588d71911
AyaAl-wahidi <ayanalwahidi@gmail.com>
2024-04-17 17:29:15 +0300

    submit lab 11

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const data = require("./MovieData/data.json");
+
+// To connect the server
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
 
 function DataFormat(Message) {
     this.Message = Message;
@@ -20,22 +24,16 @@ app.get('/favorite', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    const resData = new Movie("Spider-Man: No Way Home", "/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg", "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.");
+    const resData = new Movie(data.title, data.poster_path, data.overview);
     res.json(resData);
 });
 
-// To connect the server
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+// Error handler for server errors (status 500)
+app.use((res) =>{
+    res.status(500).json({ stutus : 500 , responseText: "Sorry, something went wrong." });
+});
 
 // Error handler for server errors (status 500)
-function handleServerError(err, req, res, next) {
-    res.status(500).json({ stutus : 500 , responseText: "Sorry, something went wrong." });
-}
-
-// Error handler for page not found (status 404)
-function handlePageNotFound(req, res, next) {
+app.use((res) =>{
     res.status(404).json({stutus : 404 , responseText: "Page not found." });
-}
-
-app.use(handlePageNotFound);
-app.use(handleServerError);
+});
